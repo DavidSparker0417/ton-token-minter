67b9eee8b7b31fb87cf75b89feff271b3c309778
DavidSparker <davidsparker0417@gmain.com>
2024-06-28 02:29:09 +0900

    updated to deploy on testnet

--- src/lib/get-ton-client.ts
+++ src/lib/get-ton-client.ts
@@ -2,13 +2,17 @@ import { TonClient } from "ton";
 import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { getNetwork } from "./hooks/useNetwork";
 
-const endpointP = getHttpEndpoint({
-  network: getNetwork(new URLSearchParams(window.location.search)),
-});
+const endpointP = "https://testnet.toncenter.com/api/v2/jsonRPC";
+const apiKey = "7a0c2bf7b733201f0f4e21d6ccebd4699375b09ea57e1aa0a428cd99687e9b21";
+
+// const endpointP = getHttpEndpoint({
+//   network: getNetwork(new URLSearchParams(window.location.search)),
+// });
 
 async function _getClient() {
   return new TonClient({
     endpoint: await endpointP,
+    apiKey: await apiKey,
   });
 }
 
